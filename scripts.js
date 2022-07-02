60294e9f6097c13c34920747db267f74b4d9b027
Naheshiko <victor.cen32@gmail.com>
2022-07-02 01:00:24 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -2,9 +2,9 @@ const twitter = document.querySelector('.twitter')
 const whatsapp = document.querySelector('.whatsapp')
 const telegram = document.querySelector('.telegram')
 
-const twitterApi = 'https://twitter.com/intent/tweet?text=Infórmate sobre el ciberfraude! https://grupocafraudecibernetico.netlify.app'
-const whatsappApi = 'https://wa.me/?text=Infórmate sobre el ciberfraude! https://grupocafraudecibernetico.netlify.app'
-const telegramApi = 'https://t.me/share/https://t.me/share/url?url=https://grupocafraudecibernetico.netlify.app&text=Infórmate sobre el ciberfraude'    
+const twitterApi = 'https://twitter.com/intent/tweet?text=Infórmate sobre el ciberfraude! https://grupocafraudecibernetico.herokuapp.com'
+const whatsappApi = 'https://wa.me/?text=Infórmate sobre el ciberfraude! https://grupocafraudecibernetico.herokuapp.com'
+const telegramApi = 'https://t.me/share/https://t.me/share/url?url=https://grupocafraudecibernetico.herokuapp.com&text=Infórmate sobre el ciberfraude'    
 
 twitter.addEventListener('click', () => {
     window.open(url = twitterApi, target='blank')
